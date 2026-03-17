06cf4521c64784f8b35944799f346d20db6cba62
bawunallah-coder <bawunallah@gmail.com>
2026-03-17 16:43:36 +0100

    Send motivational quote email on Mondays

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+import datetime as dt
+import random
+import smtplib
+import os
 
+my_email = os.environ.get("MY_EMAIL")
+password = os.environ.get("MY_PASSWORD")
+
+now = dt.datetime.now()
+week_day=now.weekday()
+
+if week_day==0:
+    with open("quotes.txt") as quote_file:
+        all_quotes=quote_file.readlines()
+        quote=random.choice(all_quotes)
+
+    with smtplib.SMTP("smtp.gmail.com",port=587) as connection:
+        connection.starttls()
+        connection.login(user=my_email,password=password)
+        connection.sendmail(from_addr=my_email,
+                            to_addrs=my_email,
+                            msg=f"Subject:Monday Motivational Quote\n\n{quote}."
+                            )
